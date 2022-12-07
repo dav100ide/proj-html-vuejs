51d0ecffda17b57a8a3020705d90776c91615c05
dav100ide <oreglio.dev@gmail.com>
2022-12-07 14:16:29 +0100

    aggiunge ResultsSection

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,18 @@
 <template>
    <main>
       <IntroSection />
+      <ResultsSection />
    </main>
 </template>
 
 <script>
 import IntroSection from './MainSections/IntroSection.vue';
+import ResultsSection from './MainSections/ResultsSection.vue';
+
 export default {
    components: {
       IntroSection,
+      ResultsSection,
    },
 };
 </script>
---
+++ src/components/MainSections/ResultsSection.vue
@@ -0,0 +1,80 @@
+<template>
+   <section>
+      <div class="overlay"></div>
+      <div class="ms_py ms_container text-center">
+         <h4 class="mb-4">WHAT ARE WE DOING</h4>
+         <h2>
+            <strong class="secondary | px-2 py-1 rounded-2"> Results </strong>
+            in Numbers
+         </h2>
+         <div class="row">
+            <div v-for="result in results" class="col-3">
+               <div class="h2">
+                  {{ result.number }}
+               </div>
+               <div class="h5">
+                  {{ result.name }}
+               </div>
+            </div>
+
+            <!-- <div class="col-3">col</div>
+            <div class="col-3">col2</div>
+            <div class="col-3">col3</div>
+            <div class="col-3">col4</div> -->
+         </div>
+      </div>
+   </section>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         results: [
+            {
+               name: 'Certifications',
+               number: 128,
+            },
+            {
+               name: 'Employees',
+               number: 230,
+            },
+            {
+               name: 'Customers',
+               number: 517,
+            },
+            {
+               name: 'Countries Served',
+               number: 94,
+            },
+         ],
+      };
+   },
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+   position: relative;
+   .overlay {
+      z-index: -1;
+      @include position-absolute-parent-size;
+      background-image: url(../../assets/city2.jpg);
+      background-size: cover;
+      background-position: bottom;
+      filter: grayscale(1);
+   }
+   h2 {
+      color: var(--neutral-color-100);
+      margin-bottom: 8rem;
+   }
+   .h2 {
+      color: var(--primary-color-300);
+      font-weight: 400;
+   }
+   .h5 {
+      color: var(--neutral-color-100);
+      font-weight: bold;
+   }
+}
+</style>
