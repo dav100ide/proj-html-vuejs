0184f82f4aa11510c78f7c8f6ca0c9dd72926b12
dav100ide <oreglio.dev@gmail.com>
2022-12-08 15:08:22 +0100

    aggiunge form sezione message

--- src/components/MainSections/MessageSection.vue
+++ src/components/MainSections/MessageSection.vue
@@ -9,10 +9,63 @@
                </h2>
                <p>We will respond to ur message on if we want to, cuz we r the best.</p>
                <!-- form message -->
-               <form class="message-form">
-                  <div class="row">
-                     <div class="col-6"></div>
+               <form @submit.prevent>
+                  <div class="row g-3 mb-3">
+                     <div class="col-6">
+                        <div class="form-floating">
+                           <input
+                              type="text"
+                              class="form-control"
+                              id="message-name"
+                              placeholder="Name"
+                           />
+                           <label for="message-name">Name</label>
+                        </div>
+                     </div>
+                     <div class="col-6">
+                        <div class="form-floating">
+                           <input
+                              type="text"
+                              class="form-control"
+                              id="message-mail"
+                              placeholder="Email"
+                           />
+                           <label for="message-mail">Email</label>
+                        </div>
+                     </div>
+                     <div class="col-6">
+                        <div class="form-floating">
+                           <input
+                              type="text"
+                              class="form-control"
+                              id="message-phone"
+                              placeholder="Phone"
+                           />
+                           <label for="message-phone">Phone</label>
+                        </div>
+                     </div>
+                     <div class="col-6">
+                        <select class="form-select">
+                           <option selected>More Info</option>
+                           <option value="1">One</option>
+                           <option value="2">Two</option>
+                        </select>
+                     </div>
+                     <div class="col-12">
+                        <div class="form-floating">
+                           <textarea
+                              class="form-control"
+                              name="message"
+                              id="message"
+                              cols="30"
+                              rows="50"
+                              placeholder="Message"
+                           ></textarea>
+                           <label for="message">Message</label>
+                        </div>
+                     </div>
                   </div>
+                  <PrimaryBtn type="submit" innerHTML="SEND" />
                </form>
                <!-- /form message -->
             </div>
@@ -23,7 +76,36 @@
 </template>
 
 <script>
-export default {};
+import PrimaryBtn from '../btns/PrimaryBtn.vue';
+export default {
+   components: {
+      PrimaryBtn,
+   },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.form-control {
+   &:focus {
+      background-color: var(--neutral-color-200);
+   }
+}
+.form-control,
+.form-select {
+   background-color: var(--neutral-color-200);
+}
+
+.form-select {
+   padding: 1rem 0.75rem;
+   border: none;
+}
+
+textarea.form-control {
+   overflow-y: auto;
+   height: 11rem;
+}
+
+button {
+   padding: 0.5rem 2rem;
+}
+</style>
